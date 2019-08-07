11d4ef59cd5600f01a72781f72c835744cd23c9a
Adam Hooper <adam@adamhooper.com>
2019-08-07 14:37:27 -0400

    Test with pandas 0.25.0

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(
-    name='libraryofcongress',
-    version='0.0.1',
-    description='Search the Library of Congress for document records',
-    author='Adam Hooper',
-    author_email='adam@adamhooper.com',
-    url='https://github.com/CJWorkbench/libraryofcongress',
-    packages=[''],
-    py_modules=['libraryofcongress'],
-    install_requires=['pandas==0.24.2', 'aiohttp']
+    name="libraryofcongress",
+    version="0.0.1",
+    description="Search the Library of Congress for document records",
+    author="Adam Hooper",
+    author_email="adam@adamhooper.com",
+    url="https://github.com/CJWorkbench/libraryofcongress",
+    packages=[""],
+    py_modules=["libraryofcongress"],
+    install_requires=["pandas==0.25.0", "aiohttp"],
 )
